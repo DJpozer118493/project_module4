ab39b6d7aa6b359813dca4c2d3d4d8f19291bea7
DJpozer118493 <137497680+DJpozer118493@users.noreply.github.com>
2023-08-16 12:59:10 +0300

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from .models import Advertisement
+from django.db.models.query import QuerySet
+# Register your models here.
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_at', 'created_date', 'update_date']
+    list_filter = ['auction', 'created_at', 'price']
+    actions = ['make_action_as_false', 'make_action_as_true']
+    fieldsets = (
+        ('Общие', {
+            "fields": (
+                'title', 'description'
+            ),
+        }),
+        ('Финансы', {
+            "fields": (
+                'price', 'auction'
+            ),
+            'classes': ['collapse']
+        })
+    )
+
+
+
+    @admin.action(description='Убрать возможность торга')
+    def make_action_as_false(self, request, queryset:QuerySet):
+        queryset.update(auction = False)
+    
+
+    @admin.action(description='Добавить возможность торга')
+    def make_action_as_true(self, request, queryset:QuerySet):
+        queryset.update(auction = True)
+
+
+
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 class Advertisement(models.Model):
-    title = models.CharField("заголовок", max_length=128) #строковое поле, используется для строк НЕБОЛЬШИХ размеров
+    title = models.CharField("заголовок", max_length=100) #строковое поле, используется для строк НЕБОЛЬШИХ размеров
     description = models.TextField("Описание") #строковое поле, используется для строк БОЛЬШИХ размеров
     price = models.DecimalField("цена", max_digits=10, decimal_places=2) #числовое поле, которое может содержать десятичное число с фиксированной точностью
     # 999.99 max_digits = 5, decimal_places = 2
@@ -9,18 +12,31 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-class Advertisements_up(models.Model):
-    def __str__(self): 
-        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
-    id = models.CharField("id", max_length=64, primary_key=True)
-    title = models.CharField("заголовок", max_length=128) #строковое поле, используется для строк НЕБОЛЬШИХ размеров
-    description = models.TextField("Описание") #строковое поле, используется для строк БОЛЬШИХ размеров
-    price = models.DecimalField("цена", max_digits=10, decimal_places=2) #числовое поле, которое может содержать десятичное число с фиксированной точностью
-    # 999.99 max_digits = 5, decimal_places = 2
-    auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен") #True and False 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True) 
-    class Meta: 
-        db_table = 'advertisements'
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:$S')
+            return format_html(
+                "<span style='color:green; font-weight: bold'>Сегодня в {}</span>",
+                created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y at %H:%M:%S')
+    
+
+    @admin.display(description='дата дата обновления')
+    def update_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            update_time = self.created_at.time().strftime('%H:%M:$S')
+            return format_html(
+                "<span style='color:blue; font-weight: bold'>Сегодня в {}</span>",
+                update_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y at %H:%M:%S')
+    
+#http://127.0.0.1:8000/admin
+    
+    
+
         
 
