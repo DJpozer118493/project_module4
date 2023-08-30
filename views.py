1433151a5b584f79b71ce19719f48cb4b34986df
DJpozer118493 <137497680+DJpozer118493@users.noreply.github.com>
2023-08-30 09:11:49 +0300

    Add files via upload

--- views.py
+++ views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Advertisement
+from .forms import AdvertisementForm
+from django.urls import reverse
 
 
-def lesson_4(request):
-    return HttpResponse("Домашка по 4 занятию")
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisement': advertisements}
+    return render(request, "index.html", context)
+
+
+def top_sellers(request):
+    return render(request, "top-sellers.html")
+
+
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_advertisement = form.save(commit=False)
+            new_advertisement.user = request.user
+            new_advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form' : form}
+    return render(request, 'advertisement-post.html', context)
+
+
+
+
 
